64a64855973f47fbaadd586fa91dfb18d04fef3a
bandhandut19 <bondon21081@gmail.com>
2025-03-10 13:38:52 +0600

    successfully detected faces

--- dataset_generator.py
+++ dataset_generator.py
@@ -8,8 +8,9 @@ webCam=cv2.VideoCapture(0)
 
 #database insertion and updation
 def insertOrUpdateIntoDatabase(Id,Name,Age):
-    connection=sqlite3.connect("initialDatabase.db")
+    connection=sqlite3.connect("employeeDatabase.db")
     cmd = "SELECT * FROM STUDENTS WHERE ID="+str(Id)
+    cursor = connection.cursor()
     cursor=connection.execute(cmd)
     isRecordExists = 0
     for row in cursor:
@@ -26,3 +27,23 @@ Id=input("Enter Employee ID")
 Name=input("Enter Employee Name")
 Age=input("Enter Employee Age")
 insertOrUpdateIntoDatabase(Id,Name,Age)
+
+
+#face detection in web cam using openCV
+sampleNumber=0
+while(True):
+    ret,employeeImg=webCam.read()
+    gray=cv2.cvtColor(employeeImg,cv2.COLOR_BGR2GRAY) #converting image color to gray
+    faces=faceDetectionHelper.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+        sampleNumber=sampleNumber+1
+        cv2.imwrite("dataset/employee."+str(Id)+"."+str(sampleNumber)+".jpg",gray[y:y+h,x:x+w])
+        cv2.rectangle(employeeImg,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.waitKey(100)
+    cv2.imshow("EmployeeFace",employeeImg)
+    cv2.waitKey(100)
+    if(sampleNumber>20):
+        break;
+
+webCam.release()
+cv2.destroyAllWindows()
